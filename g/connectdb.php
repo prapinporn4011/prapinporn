adee876e3ec0fd4e061fa1bea76d557233cde1e9
prapinporn4011 <66010914011@msu.ac.th>
2026-01-28 16:40:36 +0700

    a

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pwd  = "";
-$db   = "4038db";
+$pwd  = "Pw@1458800032693";
+$db   = "4011db";
 $conn = mysqli_connect($host, $user, $pwd, $db)
         or die("เชื่อมต่อฐานข้อมูลไม่ได้");
 mysqli_set_charset($conn, "utf8");
